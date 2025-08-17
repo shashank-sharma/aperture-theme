dcfb629907f583e2215d0662fc949eec96aeeb20
Shashank Sharma <shashank.sharma98@gmail.com>
2025-08-17 19:34:35 +0530

    Fix Deploy scripts

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'node:path';
 
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      '@shashank-sharma/aperture-theme': path.resolve(__dirname, '../src'),
+    },
+  },
 });
 
 
